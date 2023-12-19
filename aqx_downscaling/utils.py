ae64e8622b0e8113ad7ae6760b1a948064f8da2d
gtondapu <githika.tondapu@nasa.gov>
2023-12-18 19:18:00 -0500

    dates

--- aqx_downscaling/utils.py
+++ aqx_downscaling/utils.py
@@ -21,18 +21,18 @@ def get_dates(request):
             filename = x[:-3]
             date_format = '%Y%m%d'
 
-            date_obj = datetime.strptime(filename.split("_")[1], date_format)
+            date_obj = datetime.strptime(filename, date_format)
             date_arr.append( date_obj.strftime('%Y-%m-%d'))
 
-        return_obj['var1'] = filename.split("_")[0]
+        return_obj['var1'] = filename
     for x in os.listdir(file_path2):
         if x.endswith(".nc"):
             filename = x[:-3]
             date_format = '%Y%m%d'
 
-            date_obj = datetime.strptime(filename.split("_")[1], date_format)
+            date_obj = datetime.strptime(filename, date_format)
             date_arr.append(date_obj.strftime('%Y-%m-%d'))
-        return_obj['var2'] = filename.split("_")[0]
+        return_obj['var2'] = filename
     return_obj['dates'] = sorted(list(set(date_arr)))
     print(return_obj)
     return JsonResponse(return_obj)
